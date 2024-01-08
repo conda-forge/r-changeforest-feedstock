d01acf5d28323539f72635e6b4962747c51f4192
Malte Londschien <malte@londschien.de>
2024-01-08 21:49:09 +0100

    rustup target add aarch64-apple-darwin

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@ export DISABLE_AUTOBREW=1
 # https://github.com/conda-forge/r-changeforest-feedstock/issues/28
 if [[ $target_platform == osx-64 ]]; then
   export TARGET="aarch64-apple-darwin"
+  rustup target add aarch64-apple-darwin
 fi
 
 # Pass R version to libR-sys package via environment variable. This allows for cross-compilation,
