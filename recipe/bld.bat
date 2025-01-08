5a821e942fb8dbb00c8dddca7c980ffc26c6ba83
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-07 16:50:07 -0800

    remove workaround

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,2 @@
-:: Remove .def file to avoid build failure and patch in __declspec(dllexport) instead
-rm changeforest-r/src/changeforest-win.def
-sed -i -e "s/void R_init_changeforest/__declspec(dllexport) void R_init_changeforest/" changeforest/src/entrypoint.c
-
 "%R%" CMD INSTALL --build changeforest-r %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit 1
